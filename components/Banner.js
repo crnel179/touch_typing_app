b9c7c2c2aeb7d5804b410e255da0689cfc4e30db
Corneliu Laur <cl179971@gmail.com>
2023-05-18 13:59:07 +0100

    added comments to Banner component and css & worked on its responsiveness

--- components/Banner.js
+++ components/Banner.js
@@ -15,6 +15,7 @@ const Banner = () => {
             sx={{ flexGrow: 1 }}
             className={styles.banner_container}
         >
+            {/* LEFT BANNER GRID */}
             <Grid
                 container
                 spacing={0}
@@ -22,6 +23,7 @@ const Banner = () => {
                 xs={6}
                 className={styles.banner_left}
             >
+                {/* exclamation mark img (left) */}
                 <Grid xs={4} className={styles.banner_ex_mark}>
                     <Image
                         className={styles.banner_ex_mark}
@@ -31,28 +33,35 @@ const Banner = () => {
                         height={45.15}
                     />
                 </Grid>
-                <Grid xs={8}>
-                    <h3>Touch typing training for children and adults</h3>
-                    <p>
-                        <span className={styles.banner_span}>
-                            Save time and work smarter
-                        </span>
-                        &nbsp;with one of the most valuable skills you or your
-                        children can learn.
-                    </p>
-                    <p>
-                        <span className={styles.banner_span}>Touch typing</span>
-                        &nbsp;is learning to type quickly and accurately without
-                        looking at your hands using all your fingers on specific
-                        keys.
-                        <span className={styles.banner_span}>
-                            &nbsp;Join our course&nbsp;
-                        </span>
-                        and gain a hugely beneficial skill for life.
-                    </p>
+                {/* banner text (right) */}
+                <Grid xs={8} className={styles.banner_text_n_button}>
+                    <div className={styles.banner_text}>
+                        <h3>Touch typing training for children and adults</h3>
+                        <p>
+                            <span className={styles.banner_span}>
+                                Save time and work smarter
+                            </span>
+                            &nbsp;with one of the most valuable skills you or
+                            your children can learn.
+                        </p>
+                        <p>
+                            <span className={styles.banner_span}>
+                                Touch typing
+                            </span>
+                            &nbsp;is learning to type quickly and accurately
+                            without looking at your hands using all your fingers
+                            on specific keys.
+                            <span className={styles.banner_span}>
+                                &nbsp;Join our course&nbsp;
+                            </span>
+                            and gain a hugely beneficial skill for life.
+                        </p>
+                    </div>
                     <Button className={styles.banner_btn}>Our Courses</Button>
                 </Grid>
             </Grid>
+
+            {/* RIGHT BANNER GRID */}
             <Grid
                 xs={6}
                 container
@@ -60,13 +69,15 @@ const Banner = () => {
                 sx={{ flexGrow: 1 }}
                 className={styles.banner_right}
             >
+                {/* typing man img */}
                 <div className={styles.banner_img}>
                     <Image
                         src={banner_img}
                         alt='man typing on a laptop at a desk'
                     />
                 </div>
-                <div className={styles.banner_e_section}>
+                {/* 'E' letter box img */}
+                <div className={styles.banner_e_container}>
                     <Image
                         className={styles.banner_e}
                         src={banner_e}
